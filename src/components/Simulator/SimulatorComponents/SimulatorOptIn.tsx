0b82d1b090136fa376ee82772f156a216d0214c0
adityaChincholkarPinnacle <aditya.chincholkar@pinnacle.in>
2025-04-18 18:06:01 +0530

    [Fix]:In optIn card when we give init-value as true it checks in the checkbox in  mobilePreview when we give initial-value as true.

--- src/components/Simulator/SimulatorComponents/SimulatorOptIn.tsx
+++ src/components/Simulator/SimulatorComponents/SimulatorOptIn.tsx
@@ -6,7 +6,7 @@ interface Props { component: Component; }
 
 const SimulatorOptIn: React.FC<Props> = ({ component }) => (
   <FormControlLabel
-    control={<Checkbox required={component.properties?.required === 'true'} />}
+    control={<Checkbox checked={component.properties?.initValue === 'true'} required={component.properties?.required === 'true'} />}
     label={component.properties?.label || 'I agree'}
   />
 );
